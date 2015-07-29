92998f807ac1f1f3efdb7bf4c55d443656283a95
Evan Martin <martine@danga.com>
2015-07-29 09:15:35 -0700

    docs

--- keys/key.go
+++ keys/key.go
@@ -1,14 +1,19 @@
+// Package keys abstracts keyboard input, for use both in mapping X
+// events to key events and in readline's key handling.
 package keys
 
 import "fmt"
 
 //go:generate stringer -type=Sym
+
+// Sym is a key symbol: e.g. the 'a' key.
 type Sym int
 
 const (
 	SymNone Sym = 0
 
-	// 1 through 127 as in ASCII.
+	// 1 through 127 are the same as in ASCII.
+
 	Backspace Sym = 8
 	Tab       Sym = 9
 	NL        Sym = 10
@@ -28,7 +33,9 @@ const (
 )
 
 type Key struct {
-	Sym  Sym
+	Sym Sym
+
+	// Modifier mask: Control, Meta, etc.
 	Mods uint
 }
 
@@ -43,6 +50,7 @@ func (sym Sym) Name() string {
 	return sym.String()
 }
 
+// Spec returns a string specifying the key, e.g. "C-M-a" for Control+Meta+a.
 func (k Key) Spec() string {
 	spec := ""
 	if k.Mods&ModControl != 0 {
