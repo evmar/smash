49498c6641820bae216a6c56695d142a52d95865
Evan Martin <martine@danga.com>
2015-11-19 11:07:25 -0800

    split

--- smash.go
+++ smash.go
@@ -5,12 +5,8 @@ import (
 	"log"
 	"os"
 	"runtime/pprof"
-	"smash/keys"
-	"smash/ui"
 	"smash/ui/gtk"
 	"syscall"
-
-	"github.com/martine/gocairo/cairo"
 )
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
@@ -21,69 +17,6 @@ func check(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-type Window struct {
-	ui  ui.UI
-	win ui.Win
-
-	view View
-	font *MonoFont
-}
-
-type View interface {
-	GetWindow() *Window
-	Draw(cr *cairo.Context)
-	Key(key keys.Key)
-	Scroll(dy int)
-	Dirty()
-	Enqueue(f func())
-}
-
-type ViewBase struct {
-	Parent View
-}
-
-func (vb *ViewBase) GetWindow() *Window {
-	return vb.Parent.GetWindow()
-}
-
-func (vb *ViewBase) Dirty() {
-	vb.Parent.Dirty()
-}
-
-func (vb *ViewBase) Enqueue(f func()) {
-	vb.Parent.Enqueue(f)
-}
-
-func (win *Window) GetWindow() *Window {
-	return win
-}
-
-func (win *Window) Mapped() {
-	panic("x")
-}
-
-func (w *Window) Draw(cr *cairo.Context) {
-	cr.SetSourceRGB(1, 1, 1)
-	cr.Paint()
-	w.view.Draw(cr)
-}
-
-func (w *Window) Key(key keys.Key) {
-	w.view.Key(key)
-}
-
-func (w *Window) Scroll(dy int) {
-	w.view.Scroll(dy)
-}
-
-func (w *Window) Dirty() {
-	w.win.Dirty()
-}
-
-func (w *Window) Enqueue(f func()) {
-	w.ui.Enqueue(f)
 }
 
 func main() {
