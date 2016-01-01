ab62daeef9a2e7fe006e5a6f4eef177b0d903523
Evan Martin <martine@danga.com>
2016-01-01 14:52:06 -0800

    hack for the headless build tag

---
+++ ui/gtk/headless.go
@@ -0,0 +1,9 @@
+// +build headless
+
+// When you run tests with the "headless" build tag, e.g.
+//   go test -tags headless ./...
+// It complains about this directory because it then has no source files.
+// This empty file seems to work around the problem, but it seems there
+// ought to be some more satisfactory option.
+
+package gtk
