3fa2d37d17f5207c5d42f9937492ac508e21b783
Evan Martin <martine@danga.com>
2016-01-01 14:13:46 -0800

    travis: more verbosity

--- install-gocairo.sh
+++ install-gocairo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 # This script installs gocairo by running its makefile.
 # This is different than "go install ..." because the makefile
 # generates the binding code based on the symbols available in
@@ -10,5 +12,8 @@ REPO=github.com/martine/gocairo
 cd $GOPATH
 git clone --depth=1 git://$REPO $REPO
 cd $REPO
+echo $GOPATH
+pwd
+rm cairo/cairo.go  # to force rerunning the generate script
 make
 
