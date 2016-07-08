65b57bb3ed3ce56add1ec1cdd776ff1c3d85e2ba
Evan Martin <martine@danga.com>
2016-07-07 18:25:15 -0700

    start on log

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 extern crate smash;
+extern crate cairo;
+extern crate gdk;
 use smash::view;
+use smash::readline::ReadLineView;
+use smash::term::Term;
+
+struct LogEntry {
+    rl: ReadLineView,
+    term: Option<Term>,
+}
+
+impl view::View for LogEntry {
+    fn draw(&mut self, cr: &cairo::Context) {
+        self.rl.draw(cr);
+    }
+    fn key(&mut self, ev: &gdk::EventKey) {
+        if let Some(ref mut term) = self.term {
+            term.key(ev);
+        } else {
+            self.rl.key(ev);
+        }
+    }
+}
+
+// struct Log {
+//     entries: Vec<LogEntry>,
+// }
 
 fn main() {
     view::init();
@@ -7,6 +33,10 @@ fn main() {
 
     {
         let mut win = win.borrow_mut();
+        win.child = Box::new(LogEntry {
+            rl: ReadLineView::new(win.context.clone()),
+            term: None,
+        });
         win.show();
     }
 
