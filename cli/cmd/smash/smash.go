527a1013f30131ba65b4437264965e4347fc2970
Evan Martin <evan.martin@gmail.com>
2019-02-26 10:16:52 -0800

    use pty and lock around websocket

--- cli/cmd/smash/smash.go
+++ cli/cmd/smash/smash.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os/exec"
 	"path/filepath"
+	"sync"
 	"syscall"
 
 	pb "github.com/evmar/smash/proto"
 	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/websocket"
+	"github.com/kr/pty"
 )
 
 var upgrader = websocket.Upgrader{
@@ -19,24 +22,28 @@ var upgrader = websocket.Upgrader{
 	EnableCompression: true,
 }
 
-func writeMsg(conn *websocket.Conn, msg *pb.ServerMsg) error {
+type conn struct {
+	mu sync.Mutex
+	ws *websocket.Conn
+}
+
+func (c *conn) writeMsg(msg *pb.ServerMsg) error {
 	data, err := proto.Marshal(msg)
 	if err != nil {
 		return err
 	}
-	if err := conn.WriteMessage(websocket.BinaryMessage, data); err != nil {
-		return err
-	}
-	return nil
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.ws.WriteMessage(websocket.BinaryMessage, data)
 }
 
 type wsWriter struct {
-	conn *websocket.Conn
+	conn *conn
 	cell int32
 }
 
 func (w *wsWriter) Write(buf []byte) (int, error) {
-	err := writeMsg(w.conn, &pb.ServerMsg{Msg: &pb.ServerMsg_Output{&pb.Output{
+	err := w.conn.writeMsg(&pb.ServerMsg{Msg: &pb.ServerMsg_Output{&pb.Output{
 		Cell:   w.cell,
 		Output: &pb.Output_Text{string(buf[:])},
 	}}})
@@ -46,34 +53,41 @@ func (w *wsWriter) Write(buf []byte) (int, error) {
 	return len(buf), nil
 }
 
-func runCmd(conn *websocket.Conn, req *pb.RunRequest) {
+func spawn(w io.Writer, cmd *exec.Cmd) error {
+	if filepath.Base(cmd.Path) == cmd.Path {
+		// TODO: should use shell env $PATH.
+		if p, err := exec.LookPath(cmd.Path); err != nil {
+			return err
+		} else {
+			cmd.Path = p
+		}
+	}
+	f, err := pty.Start(cmd)
+	if err != nil {
+		return err
+	}
+	go func() {
+		io.Copy(w, f)
+	}()
+	return cmd.Wait()
+}
+
+func runCmd(conn *conn, req *pb.RunRequest) {
 	log.Println("run:", req)
 	cmd := &exec.Cmd{Path: req.Argv[0], Args: req.Argv}
 	cmd.Dir = req.Cwd
 	w := &wsWriter{conn: conn, cell: req.Cell}
 	exitCode := 0
-	if filepath.Base(cmd.Path) == cmd.Path {
-		if p, err := exec.LookPath(cmd.Path); err != nil {
+	if err := spawn(w, cmd); err != nil {
+		if eerr, ok := err.(*exec.ExitError); ok {
+			serr := eerr.Sys().(syscall.WaitStatus)
+			exitCode = serr.ExitStatus()
+		} else {
 			fmt.Fprintf(w, "ERROR: %s\n", err)
 			exitCode = 1
-			cmd = nil
-		} else {
-			cmd.Path = p
 		}
 	}
-	if cmd != nil {
-		cmd.Stdout = w
-		cmd.Stderr = w
-		if err := cmd.Run(); err != nil {
-			if eerr, ok := err.(*exec.ExitError); ok {
-				serr := eerr.Sys().(syscall.WaitStatus)
-				exitCode = serr.ExitStatus()
-			} else {
-				fmt.Fprintf(w, "ERROR: %s\n", err)
-			}
-		}
-	}
-	err := writeMsg(w.conn, &pb.ServerMsg{Msg: &pb.ServerMsg_Output{&pb.Output{
+	err := conn.writeMsg(&pb.ServerMsg{Msg: &pb.ServerMsg_Output{&pb.Output{
 		Cell:   w.cell,
 		Output: &pb.Output_ExitCode{int32(exitCode)},
 	}}})
@@ -83,12 +97,15 @@ func runCmd(conn *websocket.Conn, req *pb.RunRequest) {
 }
 
 func serveWS(w http.ResponseWriter, r *http.Request) error {
-	conn, err := upgrader.Upgrade(w, r, nil)
+	wsConn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return err
 	}
+	conn := &conn{
+		ws: wsConn,
+	}
 	for {
-		_, buf, err := conn.ReadMessage()
+		_, buf, err := conn.ws.ReadMessage()
 		if err != nil {
 			return err
 		}
--- cli/go.mod
+++ cli/go.mod
@@ -3,4 +3,5 @@ module github.com/evmar/smash
 require (
 	github.com/golang/protobuf v1.2.1-0.20190205222052-c823c79ea157
 	github.com/gorilla/websocket v1.4.0
+	github.com/kr/pty v1.1.3
 )
