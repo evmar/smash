26d28936b333f028a32757ad44320c40db291ea2
Evan Martin <evan.martin@gmail.com>
2019-05-01 08:19:17 -0700

    use source maps

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -3,6 +3,15 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './js/smash.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['source-map-loader']
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
