dcf4324f5460cec5b03d9e38e83dbef856bfc303
Evan Martin <evan.martin@gmail.com>
2019-05-01 07:59:54 -0700

    add test skeleton

---
+++ web/src/test.ts
@@ -0,0 +1,10 @@
+import './readline';
+import './term';
+import { expect } from 'chai';
+//import 'mocha';
+
+describe('readline', () => {
+  it('accepts emacs keys', () => {
+    expect(true).equal(true);
+  });
+});
