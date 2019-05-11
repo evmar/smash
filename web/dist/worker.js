022bef6db08cd5ca9e097c371ebdf09e44f0c0e8
Evan Martin <evan.martin@gmail.com>
2019-05-11 10:41:32 -0700

    more tinkering with pwa

--- web/dist/worker.js
+++ web/dist/worker.js
@@ -1,2 +1,5 @@
 // This is required for 'add to homescreen' to work.
-this.addEventListener('fetch', function(event) {});
+this.addEventListener('fetch', function(event) {
+  console.log('fetch', event);
+  // event.respondWith(fetch(event.request));
+});
