e6b644b676f31ded9066b0c479298892ec816fbd
Evan Martin <evan.martin@gmail.com>
2020-09-09 16:30:21 +0000

    more work on tabs

--- web/src/tabs.ts
+++ web/src/tabs.ts
@@ -1,32 +1,44 @@
 import { CellStack } from './cells';
-import { html } from './html';
+import { html, htext } from './html';
 import * as proto from './proto';
 import { Shell } from './shell';
 
 interface Tab {
+  dom: HTMLElement;
   cellStack: CellStack;
 }
 
 export class Tabs {
-  dom = html('div', { className: 'tabs' });
+  tabStrip = html('div', { className: 'tabstrip', style: { display: 'none' } });
+  dom = html('div', { className: 'tabs' }, this.tabStrip);
+
   tabs: Tab[] = [];
+  sel = -1;
   delegates = {
     send: (msg: proto.ClientMessage) => {},
   };
 
   addCells(shell: Shell) {
-    if (this.tabs.length > 0) throw new Error('multiple newclls');
-    const tab = this.newCells(shell);
+    const tab = this.newTab(shell);
     this.tabs.push(tab);
-    this.dom.appendChild(tab.cellStack.dom);
+    this.tabStrip.appendChild(tab.dom);
+
+    if (this.tabs.length > 1) {
+      this.tabStrip.style.display = 'block';
+    }
+
+    if (this.sel === -1) {
+      this.showTab(0);
+    }
   }
 
-  private newCells(shell: Shell): Tab {
+  private newTab(shell: Shell): Tab {
+    const dom = html('div', { className: 'tab' }, htext('tab'));
     const cellStack = new CellStack(shell);
     cellStack.delegates = {
       send: (msg) => this.delegates.send(msg),
     };
-    return { cellStack };
+    return { dom, cellStack };
   }
 
   handleMessage(msg: proto.ServerMsg): boolean {
@@ -42,7 +54,16 @@ export class Tabs {
     return false;
   }
 
+  showTab(index: number) {
+    if (this.sel === index) return;
+    if (this.sel >= 0) {
+      this.dom.removeChild(this.dom.lastChild!);
+    }
+    this.dom.appendChild(this.tabs[index].cellStack.dom);
+    this.sel = index;
+  }
+
   focus() {
-    this.tabs[0].cellStack.focus();
+    this.tabs[this.sel].cellStack.focus();
   }
 }
